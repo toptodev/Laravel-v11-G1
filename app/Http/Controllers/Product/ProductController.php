e7ba3dbbb88d5de5eaf799361a8138d36dbd4055
Taveevut <nakomah.web@gmail.com>
2024-08-17 14:20:52 +0700

    feat (product): ส่งค่าตัวแปร products ไปยัง views

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -7,59 +7,180 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    $products = $this->products();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    return view('products.index', compact('products'));
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(string $id)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(string $id)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(string $id)
+  {
+    //
+  }
+
+  private function products($id = "")
+  {
+    $array = [
+      [
+        'name' => 'ช็อกโกแลตบอล',
+        'price' => '10 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ลูกอมชุปช็อกโกแลต',
+        'price' => '5 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'บ๊วยห่อ',
+        'price' => '3 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'กระดาษสาหร่าย',
+        'price' => '2 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'เฮลโลคิตตี้',
+        'price' => '12 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ลูกอมมะนาว',
+        'price' => '4 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ขนมผิง',
+        'price' => '6 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ช็อกโกแลตลายทาง',
+        'price' => '8 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ลูกอมไอศกรีม',
+        'price' => '7 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'มินิเบอร์รี่',
+        'price' => '5 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'เยลลี่ผลไม้',
+        'price' => '9 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ลูกอมรสชาติเปรี้ยว',
+        'price' => '4 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ถั่วลิสงเคลือบน้ำตาล',
+        'price' => '11 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ขนมโดเรมอน',
+        'price' => '12 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ลูกอมเคี้ยวหนึบ',
+        'price' => '6 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ขนมปังปิ้งรสช็อกโกแลต',
+        'price' => '7 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ลูกอมรสหวาน',
+        'price' => '3 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'พริกป่นรสชาติขนม',
+        'price' => '5 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ขนมเหนียว',
+        'price' => '8 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ลูกอมรสเปรี้ยว',
+        'price' => '4 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ขนมกล้วย',
+        'price' => '7 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ],
+      [
+        'name' => 'ขนมเยลลี่รูปสัตว์',
+        'price' => '10 บาท',
+        'image' => 'https://placehold.co/240x240/EEE/31343C?font=lato'
+      ]
+    ];
+
+    return $id == "" ? $array : $array[$id];
+  }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,11 @@
 <h1>สินค้าของเรา</h1>
+
+<table border="1">
+@foreach ($products as $product)
+		<tr>
+				<td>{{ $product['name'] }}</td>
+				<td>{{ $product['price'] }}</td>
+				<td><img src="{{ $product['image'] }}"></td>
+		</tr>
+@endforeach
+</table>
