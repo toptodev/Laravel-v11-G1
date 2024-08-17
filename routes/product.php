d21e475798baedaddf45087269f986331f4a7fc4
Taveevut <nakomah.web@gmail.com>
2024-08-17 21:52:19 +0700

    create (routes): สร้างเส้นทางสำหรับ admin

--- routes/product.php
+++ routes/product.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Product\Admin\ProductController as AdminProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('products', ProductController::class);
+
+Route::prefix('admin')->name('admin.')->group(function () {
+  Route::resource('products', AdminProductController::class);
+});
