73b62f58a17a00da350dbc1e3828a4e1a26c6267
Taveevut <nakomah.web@gmail.com>
2024-08-17 12:02:16 +0700

    feat (product): สร้างเส้นทางแบบง่าย

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Product\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/create', [ProductController::class, 'create']);
+Route::post('products', [ProductController::class, 'store']);
+Route::get('products/{product}', [ProductController::class, 'show']);
+Route::get('products/{product}/edit', [ProductController::class, 'edit']);
+Route::put('products/{product}', [ProductController::class, 'update']);
+Route::delete('products', [ProductController::class, 'destroy']);
