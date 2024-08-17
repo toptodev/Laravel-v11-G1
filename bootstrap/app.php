e3561bde1d971c0975486e4fd25e07318ae4eef5
Taveevut <nakomah.web@gmail.com>
2024-08-17 19:49:29 +0700

    fix (routes) : สร้างเส้นทางแบบแยกไฟล์

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Router;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+  ->withRouting(
+    function (Router $router) {
+      $router->middleware('web')->group(base_path('routes/web.php'));
+      $router->middleware('web')->group(base_path('routes/product.php'));
+    },
+    commands: __DIR__ . '/../routes/console.php',
+  )
+  ->withMiddleware(function (Middleware $middleware) {
+    //
+  })
+  ->withExceptions(function (Exceptions $exceptions) {
+    //
+  })->create();
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\Product\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('products', ProductController::class);
