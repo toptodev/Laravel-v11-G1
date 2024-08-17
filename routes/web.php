c6eb1a32b1ddbf28415044ba9dfd96de4216dd7d
Taveevut <nakomah.web@gmail.com>
2024-08-17 12:03:04 +0700

    feat (product): กำหนดเส้นทาง routes แบบย่อ

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('products', [ProductController::class, 'index']);
-Route::get('products/create', [ProductController::class, 'create']);
-Route::post('products', [ProductController::class, 'store']);
-Route::get('products/{product}', [ProductController::class, 'show']);
-Route::get('products/{product}/edit', [ProductController::class, 'edit']);
-Route::put('products/{product}', [ProductController::class, 'update']);
-Route::delete('products', [ProductController::class, 'destroy']);
+Route::resource('products', ProductController::class);
